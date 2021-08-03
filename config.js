8ccb6308a06762ef9f3d13dc9b539bcc3892e581
ANGGER4 <85092224+ANGGER4@users.noreply.github.com>
2021-08-03 10:58:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012'] // Letakan nomor kamu disini
+global.owner = ['62851563346420'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -25,8 +25,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = '❤️'
-global.author = 'ariffb'
+global.packname = 'Created By'
+global.author = 'angger bot'
 
 global.wait = '_*tunggu..*_'
 
